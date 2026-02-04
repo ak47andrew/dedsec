6c7e94ee84bbd12874485ac6a101492d38e71550
AK-47 <161627201+ak47andrew@users.noreply.github.com>
2026-02-04 18:36:14 +0300

    123

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,7 +1,7 @@
 URL=https://github.com/ak47andrew/dedsec/releases/latest/download/dedsec-x86_64-unknown-linux-gnu
 TMP_DIR=$(mktemp -d)
 FILENAME="$TMP_DIR/app"
-curl -L -o "$FILENAME" $URL
+curl -L -o "$FILENAME" $URL > /dev/null
 chmod +x "$FILENAME"
 clear
 "$FILENAME"
