15db1d2a35ce1b6191b07c65cf0f50e0605699e5
AK-47 <161627201+ak47andrew@users.noreply.github.com>
2026-02-04 18:07:35 +0300

    bootstrap.sh

---
+++ bootstrap.sh
@@ -0,0 +1,8 @@
+URL=https://github.com/ak47andrew/dedsec/releases/latest/download/dedsec-x86_64-unknown-linux-gnu
+TMP_DIR=$(mktemp -d)
+FILENAME="$TMP_DIR/app"
+curl -L -o "$FILENAME" $URL
+chmod +x "$FILENAME"
+clear
+"$FILENAME"
+rm -rf TMP_DIR
